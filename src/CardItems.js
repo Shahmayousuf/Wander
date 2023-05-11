782ea66b483be16d893da7f3399dce0763d37989
Shahmayousuf <shahmayousuf@gmail.com>
2023-05-11 08:35:07 +0530

    card component

---
+++ src/CardItems.js
@@ -0,0 +1,42 @@
+
+
+
+
+export const CARDITEMS=[
+    {
+        src:'Images/img-3.jpg',
+        text :'Set sail in the Atlantic Ocean visiting Uncharted Waters',
+        label:'Mystery',
+        path:'/services'
+
+    },
+    {
+        src:'Images/img-4.jpg',
+        text:'Experience Football on the top of the Himalayan mountains',
+        label:'Adventure',
+        path:'/products'
+
+    },
+    {
+        src:'/Images/img-8.jpg',
+        text:'Ride through the Sahara desert on a guided',
+        label:'Adventure',
+        path:'/sign-up'
+
+    },
+    {
+    src:'Images/img-9.jpg',
+    text:'Explore the hidden waterfall deep inside the Amazon Jungle',
+    label:'Adventure',
+    path:'/services'
+
+    },
+    {
+    src:'Images/img-2.jpg',
+    text:'Travel through the Islands of Bali in a Private Cruise',
+    label:'Luxury',
+    path:'/services'
+    }
+
+    
+]
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,19 +1,23 @@
-import React from 'react'
-import CardItem from './CardItem'
-import './Card.css'
+import React from 'react';
+import './Card.css';
+import CardItem from './CardItem';
+import { CARDITEMS } from '../CardItems';
+
 const Card = () => {
   return (
-    <div className='cards'>
-    <h1>Check out these EPIC Destination</h1>
+    <div className="cards">
+      <h1>Check out these EPIC Destinations</h1>
       <div className="cards__container">
         <div className="cards__wrapper">
-            <ul className="cards__items">
-          <CardItem/>
-            </ul>
+          <ul className="cards__items">
+            {CARDITEMS.map((item, index) => (
+              <CardItem key={index} {...item} />
+            ))}
+          </ul>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
